0e02aff2c93668ceddc906593d767346a4d6b089
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-02-01 13:47:04 +0530

    Create a_Inserting_element.cpp
    Program to insert elements by user in a user given size of array.

---
+++ CppEssentialsHub/Data Structures and Algorithms /a_Arrays /a_Dynamic_Array /a_Inserting_element.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of array: -" << endl;
+    cin >> n;
+
+    int *arr = (int*)malloc(n*sizeof(int));
+
+    for (int i = 0; i < n ; i++)
+    {
+        cout << "Enter "<< i+1 << "element: ";
+        cin >> arr[i];
+    }
+    return 0;
+}
