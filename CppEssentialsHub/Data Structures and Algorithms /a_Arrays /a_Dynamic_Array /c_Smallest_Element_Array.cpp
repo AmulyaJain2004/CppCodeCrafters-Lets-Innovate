a7be1ba5f70e4d81b2e50feb769f0e78b4a4ed17
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-02-02 23:42:24 +0530

    Create c_Smallest_Element_Array.cpp
    Program to find the smallest element in the array and its position and displaying them.

---
+++ CppEssentialsHub/Data Structures and Algorithms /a_Arrays /a_Dynamic_Array /c_Smallest_Element_Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: -" << endl;
+    cin >> n;
+
+    float *arr = (float*)malloc(n*sizeof(int));
+
+    cout<< "Entering numbers in array: -"<< endl;
+    for (int i = 0; i < n ; i++)
+    {
+        cout << "Enter "<< i+1 << " element: ";
+        cin >> arr[i];
+    }
+
+    float min_element = arr[0];
+    int min_position = 0;
+    // Linear Search 
+    for (int i = 1; i < n ; i++)
+    {
+        if (arr[i]<min_element)
+        {
+            min_element = arr[i];
+            min_position = i;
+        }
+    }
+
+    cout  << "Smallest element in Entered element is "<< min_element <<" and it's position is " << min_position+1 <<endl;
+    return 0;
+}
