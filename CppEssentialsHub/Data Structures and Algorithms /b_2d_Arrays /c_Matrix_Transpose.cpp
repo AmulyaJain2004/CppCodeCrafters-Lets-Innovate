691a15db9988e8740a687425cde134cd3a2c27ed
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-02-10 22:54:10 +0530

    Update c_Matrix_Transpose.cpp
    Program to display Transpose of a matrix or 2d array for which its element is given by user.

--- CppEssentialsHub/Data Structures and Algorithms /b_2d_Arrays /c_Matrix_Transpose.cpp
+++ CppEssentialsHub/Data Structures and Algorithms /b_2d_Arrays /c_Matrix_Transpose.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m,n;
 
+    cout<< "Enter number of rows and columns for 2d array: -" << endl;
+    cin >> m >> n;
+
+    int arr[m][n];
+    
+    for (int i  = 0; i < m ; i++)
+    {
+        for (int j =0; j < n; j++)
+        {
+            cout << "Enter element: ";
+            cin >> arr[i][j];
+        }
+    }
+    for (int i  = 0; i < m ; i++)
+    {
+        for (int j =0; j < n; j++)
+        {
+            cout << arr[i][j] << "\t" ;
+        }
+        cout << endl;
+    }
+
+    int transp_arr[n][m];
+    cout << "Transposed Array is: - " << endl;
+    for (int i  = 0; i< m ; i++)
+    {
+        for (int j =0; j< n; j++)
+        {
+            transp_arr[j][i] = arr[i][j];
+        }
+        cout << endl;
+    }
+
+    for (int i  = 0; i< n ; i++)
+    {
+        for (int j =0; j< m; j++)
+        {
+            cout << transp_arr[i][j] << "\t" ;
+        }
+        cout << endl;
+    }
+    return 0;
+}
