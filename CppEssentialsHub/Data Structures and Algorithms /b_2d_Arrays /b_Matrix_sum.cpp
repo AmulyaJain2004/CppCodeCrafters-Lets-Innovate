3fa9287f92cf42029f112071d3c8da83d453b8ab
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-02-10 22:51:21 +0530

    Update b_Matrix_sum.cpp
    Program to take input in 2 matrices (2d arrays) using loops and displaying their matrix sum in another 2d array.

--- CppEssentialsHub/Data Structures and Algorithms /b_2d_Arrays /b_Matrix_sum.cpp
+++ CppEssentialsHub/Data Structures and Algorithms /b_2d_Arrays /b_Matrix_sum.cpp
@@ -7,21 +7,35 @@ int main()
     cout<< "Enter number of rows and columns for 2d array: -" << endl;
     cin >> m >> n;
 
-    int arr[m][n];
+    int arr1[m][n], arr2[m][n], arr3[m][n];
     
+    cout << "Enter elements in array 1: -" << endl;
     for (int i  = 0; i< m ; i++)
     {
         for (int j =0; j< n; j++)
         {
-            cout << "Enter element: ";
-            cin>> arr[i][j];
+            cout << "Enter " << i+1 << " element: ";
+            cin>> arr1[i][j];
         }
     }
+
+    cout << "Enter elements in array 2: -" << endl;
     for (int i  = 0; i< m ; i++)
     {
         for (int j =0; j< n; j++)
         {
-            cout << arr[i][j] << "\t" ;
+            cout << "Enter " << i+1 << " element: ";
+            cin>> arr2[i][j];
+        }
+    }
+
+    for (int i  = 0; i< m ; i++)
+    {
+        for (int j =0; j< n; j++)
+        {
+            arr3[i][j] = 0;
+            arr3[i][j] = arr1[i][j] + arr2[i][j];
+            cout << arr3[i][j] << " " ;
         }
         cout << endl;
     }
