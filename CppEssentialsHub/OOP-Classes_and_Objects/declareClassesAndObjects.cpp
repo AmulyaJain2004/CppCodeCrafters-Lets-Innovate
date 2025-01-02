0f7c79e18ac1ca12be43181962ee99a17e098785
Amulya Jain <jainamulyawin@gmail.com>
2025-01-02 12:26:51 +0530

    Update files

--- CppEssentialsHub/OOP-Classes_and_Objects/declareClassesAndObjects.cpp
+++ CppEssentialsHub/OOP-Classes_and_Objects/declareClassesAndObjects.cpp
@@ -7,17 +7,43 @@ using namespace std;
 class Player 
 {
     // attributes
-    string name;
-    int health;
-    int xp;
+    string name {"Player"};
+    int health {100};
+    int xp {3};
 
     // methods
     void talk(string);
     bool is_dead();
 };
 
+class Account 
+{
+    // attributes
+    string name {"Account"};
+    double balance {0.0};
+
+    // methods
+    bool deposit(double);
+    bool withdraw(double);
+};
+
 int main()
 {
+    Account frank_account;
+    Account jim_account;
+
     Player frank;
     Player hero;
+
+    Player players[] {frank, hero};
+
+    vector<Player> player_vec {frank};
+    player_vec.push_back(hero);
+
+    Player *enemy {nullptr};
+    enemy = new Player;
+
+    delete enemy;
+
+    return 0;
 }
